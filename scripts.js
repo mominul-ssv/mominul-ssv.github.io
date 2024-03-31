55f70b415b5292442c3c9dcd5baab673505070f4
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2024-03-31 14:49:59 +0600

    improve design

--- scripts.js
+++ scripts.js
@@ -2,25 +2,31 @@ function goToPage(page) {
     window.location.href = page;
 }
 
-// popup trigger
+// Check if popupOverlay exists before accessing it
 const popupOverlay = document.getElementById('popup');
-const closeButton = document.getElementById('close-button');
-const popupTriggers = document.querySelectorAll('.popup-trigger');
-const popupImage = document.querySelector('.popup-image');
+if (popupOverlay) {
+    const closeButton = document.getElementById('close-button');
+    const popupTriggers = document.querySelectorAll('.popup-trigger');
+    const popupImage = document.querySelector('.popup-image');
 
-popupTriggers.forEach(trigger => {
-    trigger.addEventListener('click', (event) => {
-        if (window.innerWidth >= 768) {
-            popupImage.src = event.currentTarget.querySelector('img').src;
-            popupOverlay.style.display = 'flex';
-        }
+    // Check if closeButton exists before adding event listener
+    if (closeButton) {
+        closeButton.addEventListener('click', () => {
+            popupOverlay.style.display = 'none';
+        });
+    }
+
+    popupTriggers.forEach(trigger => {
+        trigger.addEventListener('click', (event) => {
+            if (window.innerWidth >= 768) {
+                popupImage.src = event.currentTarget.querySelector('img').src;
+                popupOverlay.style.display = 'flex';
+            }
+        });
     });
-});
 
-closeButton.addEventListener('click', () => {
-    popupOverlay.style.display = 'none';
-});
+    popupImage.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+}
 
-popupImage.addEventListener('click', (event) => {
-    event.stopPropagation();
-});
